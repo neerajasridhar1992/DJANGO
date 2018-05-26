72507514f005ed65bc36276050642baf0669a193
Neeraja Sridhar <nerajasridhar1992@gmail.com>
2018-05-25 22:17:31 -0700

    DJANGO_Projects

---
+++ djangovm/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/neerajas/Desktop/Personal_Development_Training/djangoprojects/djangovm/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
